9466c554270c6593248901912aa55908c9f8f360
Raphael Gaias Wrublewski <raphael.gaias@tacla.corp>
2025-11-06 11:27:47 -0300

    fazer vizinho mais prox

--- kd-tree/main.c
+++ kd-tree/main.c
@@ -3,47 +3,36 @@
 
 #include "kdtree.h"
 
+
 int main(){
 	//ATENÇÃO, ESSE É APENAS UM EXEMPLO DE IMPLEMENTAÇÃO DO MAIN.
 	//MODIFIQUE DE ACORDO COM SUAS NECESSIDADES E DE ACORDO COM AS ESPECIFICAÇÕES.
 	struct tree *t = malloc (sizeof (struct tree));
+	int n;
 	if (!t) return 1;
-	
 	t->raiz = NULL;
 
 	imprimirDadosAluno();
 
-	char op;
-	scanf(" %c", &op);
+	scanf ("%d", &n);
 	scanf ("%d", &t->num_dims);
 	int val[t->num_dims];
 	
+	if (n % t->num_dims != 0) return 1;		// num de nodos não é múltiplo do num de dimensões
+
+	printf ("Insere:\n");
+	for (size_t i = 0; i < n; i + t->num_dims){
+		for (size_t j = 0; j < t->num_dims; j++){
+			scanf("%d", &val[j]);
+		}
+		if(inserir(t, val) == NULL)
+		fprintf(stderr,"Falha ao inserir.\n");
+	}
+
+	char op;
+	scanf(" %c", &op);
 	while(op != 'f'){
 		switch (op) {
-			case 'i':
-				printf ("Insere:\n");
-				for (size_t i = 0; i < t->num_dims; i++){
-					scanf("%d", &val[i]);
-				}
-				if(inserir(t, val) == NULL)
-					fprintf(stderr,"Falha ao inserir.\n");
-				break;
-			case 'r':
-				for (size_t i = 0; i < t->num_dims; i++){
-					scanf("%d", &val[i]);
-				}
-				if(!excluir(t, val)){
-					fprintf(stderr,"Falha ao remover (");
-					for (size_t i = 0; i < t->num_dims; i++){
-						printf(" %d", val[i]);
-					}
-					printf (")\n");
-				}
-				break;
-			case 'e':
-				imprimirEmOrdem(t);
-				puts("\n");
-				break;
 			case 'l':
 				imprimirEmLargura(t);
 				break;
@@ -62,6 +51,7 @@ int main(){
 				else
 					printf("Nao encontrado (%d %d)\n", val[0], val[1]);
 				break;
+			case 'z':
 			default:
 				fprintf(stderr,"Opcao Invalida %d", (int)op);
 		}
